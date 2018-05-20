8f99d3ddaebaa8caf18a5f4506463a58384463eb
w4a2y4 <b04902079@ntu.edu.tw>
2018-05-20 14:14:35 +0800

    advanced OpenMP done

--- PP-OpenMP-advanced/hw/seq.c
+++ PP-OpenMP-advanced/hw/seq.c
@@ -2,28 +2,30 @@
 #define MAXN 2048
 #define INF (1LL<<60)
 int N;
-long long dp[MAXN*MAXN] = {0}, SZ[MAXN+1];
+long long dp[MAXN*MAXN], SZ[MAXN+1], dp2[MAXN*MAXN];
 
 int main() {
     while (scanf("%d", &N) == 1) {
         for (int i = 0; i <= N; i++)
             scanf("%lld", &SZ[i]);
+        #pragma omp parallel for
         for (int i=0; i<N; i++)
             dp[i*N + i] = 0;
- 
+
+        #pragma omp parallel
         for (int i = 1; i < N; i++) {
+            #pragma omp for
             for (int j = 0; j < N-i; j++) {
-                int l = j, r = j+i;
-                long long local = INF;
-                for (int k = l; k < r; k++) {
-                    long long t = dp[l*N+k] + dp[(k+1)*N+r] + SZ[l] * SZ[k+1] * SZ[r+1];
-                    if (t < local)
-                        local = t;
+                int r = j+i;
+                long long local = INF, mult = SZ[j] * SZ[r+1];
+                for (int k = j; k < r; k++) {
+                    long long t = dp[j*N+k] + dp[r*N+k+1] + mult * SZ[k+1];
+                    if (t < local) local = t;
                 }
-                dp[l*N+r] = local;
+                dp[j*N+r] = dp[r*N+j] = local;
             }
         }
-        printf("%lld\n", dp[0*N+N-1]);
+        printf("%lld\n", dp[N-1]);
     }
     return 0;
 }
